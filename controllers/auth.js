020463ca79ea7dcdce6131499756f3cddbc44a26
yaokaoya <mahoagray@gmai.com>
2020-10-19 23:45:17 +0900

    completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -71,7 +71,10 @@ const login = (req, res) => {
           if (err) {
             return res.status(403).json({ success: false, error: err.message });
           }
-          res.cookie("jwt_auth", user.token).status(200).json(user);
+          res
+            .cookie("jwt_auth", user.token, { httpOnly: true })
+            .status(200)
+            .json(user);
         });
       });
     });
